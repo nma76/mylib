c4d40df5ea64eadd4f1081bce78257fb04a72892
Jonas Lindau <jonas@jonika.nu>
2026-07-09 13:14:32 +0200

    added my_getchar

--- include/mylib.h
+++ include/mylib.h
@@ -12,5 +12,6 @@ ssize_t my_write(int fd, const void *buf, size_t count);
 ssize_t my_read(int fd, void *buf, size_t count);
 int my_putchar(int c);
 int my_puts(const char *str);
+int my_getchar(void);
 
 #endif
---
+++ tests/test_my_getchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "mylib.h"
+
+int main(void)
+{
+    // print character
+    int r = my_getchar();
+    printf("\nReturn value was: %d\n", r);
+
+    return 0;
+}
