8f088dead45da3fcac429700eed7ec3a122aa840
Jonas Lindau <jonas@jonika.nu>
2026-07-15 08:11:02 +0200

    WIP: First version of itoa

--- tests/test_my_itoa.c
+++ tests/test_my_itoa.c
@@ -3,5 +3,22 @@
 
 int main(void)
 {
+    // value to convert to string
+    int source = 123;
+
+    // destination buffer
+    char dest[33];
+
+    // result of conversion
+    bool result = false;
+
+    // do conversion
+    result = my_itoa(source, dest);
+
+    // print results
+    printf("Integer to convert is %i\n", source);
+    printf("Returned value is %d\n", result);
+    printf("Converted string is: %s\n", dest);
+
     return 0;
 }
